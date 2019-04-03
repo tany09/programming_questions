4068151a91d9c34a30d755be7318cdd6a97b7cad
Tanveer Singh Dhawan <tanysingh09@gmail.com>
2019-04-04 02:17:35 +0530

    Reverse string added

---
+++ reversestring.js
@@ -0,0 +1,29 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+// function reverse(str) {
+//     const length = str.length;
+//     const temp = Array(length).fill().map((element, index) => {
+//         return str[index];
+//     });
+//     let newstr = '';
+//     for(let i= temp.length - 1; i >= 0; i--) {
+//         newstr = newstr + temp[i];
+//     }
+//     return newstr;
+// }
+
+function reverse(str) {
+    const arr = str.split('');
+    let rev = '';
+    for(let char of str) {
+        rev = char + rev;
+    }
+    return rev;
+}
+
